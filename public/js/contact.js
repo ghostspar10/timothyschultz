f0e4efe2fde4daf90a773b377dcc6e14373754ca
ghostspar10 <46792290+ghostspar10@users.noreply.github.com>
2021-05-02 09:13:26 -0600

    added some node stuff and rearranged folders

---
+++ public/js/contact.js
@@ -0,0 +1,5 @@
+function validation() {
+    var name = document.getElementById("name").value;
+    var name = document.getElementById("email").value;
+    var name = document.getElementById("message").value;
+}
