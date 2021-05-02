44942c951185afac940db753ab309d50caaf50e3
ghostspar10 <46792290+ghostspar10@users.noreply.github.com>
2021-05-02 09:15:34 -0600

    Revert "added some node stuff and rearranged folders"
    This reverts commit f0e4efe2fde4daf90a773b377dcc6e14373754ca.

--- public/js/contact.js
+++
@@ -1,5 +0,0 @@
-function validation() {
-    var name = document.getElementById("name").value;
-    var name = document.getElementById("email").value;
-    var name = document.getElementById("message").value;
-}
